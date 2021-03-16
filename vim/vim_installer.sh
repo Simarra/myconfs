d84d7574cd33dfd115788c04e22ebe60358bfd4b
MARTEL, Loïc <loic.martel@cgi.com>
2021-03-16 17:11:17 +0100

    Coc completion

--- vim/vim_installer.sh
+++ vim/vim_installer.sh
@@ -14,6 +14,4 @@ sudo dnf install -y curl
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+curl 'https://raw.githubusercontent.com/Simarra/myconfs/master/vim/init.vim' >> ~/.config/nvim/init.vim
