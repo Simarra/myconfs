00d561263d9f73d1c701633c277cc7a02cf7dc7a
Simarra <loic.martel@protonmail.com>
2021-09-27 20:01:50 +0200

    Update default.nix

--- cozy-drive/default.nix
+++ cozy-drive/default.nix
@@ -5,12 +5,12 @@
 
 let
   pname = "cozydrive";
-  version = "3.29.0";
+  version = "3.30.0";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://github.com/cozy-labs/cozy-desktop/releases/download/v${version}/Cozy-Drive-${version}-x86_64.AppImage";
-    sha256 = "12vjk1rxljjg14hm66iyqda3a5rgs5s7vsss0s6kf8v0yr7czc84";
+    sha256 = "1slgsjm616063kaz47437s2zaq9zl08239nwmrlgxf6873x1y33d";
   };
   appimageContents = appimageTools.extract { inherit name src; };
 
@@ -19,18 +19,17 @@ appimageTools.wrapType2 {
   inherit name src;
   extraInstallCommands = ''
     mv $out/bin/${name} $out/bin/${pname}
-    install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
-    substituteInPlace $out/share/applications/${pname}.desktop \
+    install -m 444 -D ${appimageContents}/cozydrive.desktop -t $out/share/applications
+    substituteInPlace $out/share/applications/cozydrive.desktop \
       --replace 'Exec=AppRun' 'Exec=${pname}'
     cp -r ${appimageContents}/usr/share/icons $out/share
   '';
-
 
   meta = with lib; {
     description = "Cozy Drive is a synchronization tool for your files and folders with Cozy Cloud.";
     homepage = "https://cozy.io";
     license = licenses.gpl3Only;
-    maintainers = with maintainers; [ Simarra ];
+    maintainers = with maintainers; [ "Simarra" ];
     platforms = [ "x86_64-linux" ];
   };
 }
